dee1aedadec67f80a8f04466d55e414f0ba3df01
U-BSD\519819 <519819@SHS-GYBF6X3.beaverton.k12.or.us>
2025-09-10 11:44:59 -0700

    Updated palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -7,14 +7,15 @@ char* strip(char*str){
   // strips a char array of its punctuation, and makes it lowercase
   char str2[81];
   int i = 0;
-  while (str[i] != '\0'){
-    if (str[i] != '\0'){
-      str2[80-i] = str[i]; // test palindrome, move to palindrome() function later
-      i++;
-    }
+  int len = strlen(str);
+  for (int i = 0; i < strlen(str); i++){
+    str2[len-i-1] = str[i]; // test palindrome, move to palindrome() function later
+    
   }
-  std::cout << str2 << std::flush;
-  return str; // update this to return the processed string
+  str2[len] = '\0';
+  char* pointer = new char[81]; // this needs to return a pointer so it doesn't rely on local memory
+  strcpy(pointer, str2);
+  return pointer; // update this to return the processed string
 }
 
 
@@ -23,6 +24,7 @@ int main(){
   std::cout << "Enter a phrase: " << std::flush;
   cin.get(str, 80);
   char* str2 = strip(str);
+  std::cout << str2;
   return 0;
 }
  
