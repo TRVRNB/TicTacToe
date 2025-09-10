004dfe226a56e720788a85bca2d649938cf91bb3
U-BSD\519819 <519819@SHS-GYBF6X3.beaverton.k12.or.us>
2025-09-10 11:16:07 -0700

    Updated palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -3,7 +3,26 @@
 
 using namespace std;
 
-int main(){
+char* strip(char*str){
+  // strips a char array of its punctuation, and makes it lowercase
+  char str2[81];
+  int i = 0;
+  while (str[i] != '\0'){
+    if (str[i] != '\0'){
+      str2[80-i] = str[i]; // test palindrome, move to palindrome() function later
+      i++;
+    }
+  }
+  std::cout << str2 << std::flush;
+  return str; // update this to return the processed string
+}
 
+
+int main(){
+  char str[81]; // 1 extra length so the input doesn't write over the terminating char, is that how this works?
+  std::cout << "Enter a phrase: " << std::flush;
+  cin.get(str, 80);
+  char* str2 = strip(str);
   return 0;
 }
+ 
